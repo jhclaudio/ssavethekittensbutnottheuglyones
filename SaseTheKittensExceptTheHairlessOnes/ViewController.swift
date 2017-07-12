8c0b3ac8362c08f52c6fac7b9693caba01051457
Jorge Claudio <jorge@honimark.com>
2017-07-12 11:57:43 -0400

    Initial Commit

---
+++ SaseTheKittensExceptTheHairlessOnes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaseTheKittensExceptTheHairlessOnes
+//
+//  Created by Jorge Claudio on 7/12/17.
+//  Copyright © 2017 Jorge Claudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
